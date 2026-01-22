1234b13b4ab648b209b14a75b1ad41f573b03b4d
waseemAnsari11011 <waseemahm11011@gmail.com>
2026-01-22 12:10:54 +0530

    abc

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 // Replace with your machine's IP address if testing on a real device
-// const BASE_URL = 'http://10.0.2.2:8000'; // For Android Emulator
-// const BASE_URL = 'http://localhost:8000'; // For iOS Simulator
-// const BASE_URL = 'http://192.168.x.x:8000'; // For real device (replace with your IP)
-const BASE_URL = 'https://server.sevabazar.com/';
+const BASE_URL = 'https://server.sevabazar.com/'; // For real device (replace with your IP)
+
 const apiClient = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
